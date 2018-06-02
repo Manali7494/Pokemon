e03ec02be4d588de7e608deceb5ff9b0c5ba16e9
Andrew <andrewgdewar@gmail.com>
2018-06-02 16:17:17 -0600

    successfully allowed two sessions to run and access one server

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const knex = require("knex")(knexConfig);
 
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8071;
 
 app.use(cookieSession({
     secret: "userID"
