870957c5903f254f6d22775baed9da893e215fc7
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-06-04 04:05:11 +0000

    modified seeds, app.js for scripts, profile for css

--- db/seeds/01_pokemon.js
+++ db/seeds/01_pokemon.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('pokemon').insert([
         {name: 'Squirtle', pokedex_num: 7, health: 100, attack: 20 , type: 'water', rarity: 'common', imgurl: 'http://static.pokemonpets.com/images/monsters-images-300-300/7-Squirtle.png'},
         {name: 'Charmander', pokedex_num: 4, health: 100, attack: 20, type: 'fire', rarity: 'common', imgurl: 'https://vignette.wikia.nocookie.net/pokemon-revolution/images/4/41/004Charmander_OS_anime_2.png/revision/latest?cb=20150625082016'},
-        {name: 'Bulbasaur', pokedex_num: 1, health: 100, attack: 20, type: 'grass', rarity: 'common', imgurl: 'http://'}
+        {name: 'Bulbasaur', pokedex_num: 1, health: 100, attack: 20, type: 'grass', rarity: 'common', imgurl: 'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/1200px-001Bulbasaur.png'}
       ]);
     });
 };
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,59 @@
-$(document).ready(function(){
-$("#statsTable").hide()
+/*$(document).ready(function(){
+$("#statsTable").hide();
+
+$.get("/multigame", function(data){
   $("#stats").click(function(){
     $("#central").hide();
-    $("#statsTable").show()
-    //.css("visibility","hidden");
-
+    $("#statsTable").show();
   });
+});*/
+
+
+$(document).ready(function(){
+  $("#statsTable").hide();
+  $.get("/pokemon", function(pokeList){
+    $.get("/user", function(userInfo){
+      var pokeNum = userInfo[0].pokedex_num;
+      for (var pokemon of pokeList){
+        if (pokemon.pokedex_num === pokeNum){
+          var imgPic = pokemon.imgurl;
+          $("#pokePic").attr("src", imgPic);
+        }
+        }
+    });
+  });
+
+var index = 0;
+  $.get("/multistats", function(array){
+    $.get("/myusername", function(username){
+    $("#stats").click(function(){
+    for (var i of array){
+    index += 1;
+    if (index <= array.length){
+    $("#tableS").append(`<tr> <th> ${index} </th> <th>${i.user1_name} </th> <th> ${i.user2_name}</th> <th> ${i.multi_winner}</tr>`);
+    }
+    }
+        $("#central").hide();
+    $("#statsTable").show();
+    });
+
+    });
+  });
+
+    $.get("/gamesSorted",function(array){
+    $.get("/myusername", function(username){
+      $("#stats").click(function(){
+        array.forEach(function(element, index){
+          if (username === element.username){
+            rank = index + 1;
+          }
+        });
+        $('.numWins').text(`Your rank is: ${rank}`);
+    });
+
+    });
+    });
+
 
 
 });
