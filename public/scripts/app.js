16d09c78451309acab1463f3b0fc03ca1b6888f8
Manali7494 <manalibhattacharyya10@gmail.com>
2018-06-04 17:41:49 -0600

    linted app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,44 +1,47 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
   $("#statsTable").hide();
-  $.get("/pokemon", function(pokeList){
-    $.get("/user", function(userInfo){
+  $.get("/pokemon", function(pokeList) {
+    $.get("/user", function(userInfo) {
       var pokeNum = userInfo[0].pokedex_num;
-      for (var pokemon of pokeList){
-        if (pokemon.pokedex_num === pokeNum){
+      for (var pokemon of pokeList) {
+        if (pokemon.pokedex_num === pokeNum) {
           var imgPic = pokemon.imgurl;
           $("#pokePic").attr("src", imgPic);
         }
+      }
+    });
+  });
+
+  var index = 0;
+  $.get("/multistats", function(array) {
+    $.get("/myusername", function(username) {
+      $("#stats").click(function() {
+        for (var i of array) {
+          index += 1;
+          if (index <= array.length) {
+            $("#tableS").append(
+              `<tr> <th>${i.user1_name} </th> <th> ${i.user2_name}</th> <th> ${
+                i.multi_winner
+              }</tr>`
+            );
+          }
         }
-    });
-  });
-
-var index = 0;
-  $.get("/multistats", function(array){
-    $.get("/myusername", function(username){
-    $("#stats").click(function(){
-    for (var i of array){
-    index += 1;
-    if (index <= array.length){
-    $("#tableS").append(`<tr> <th>${i.user1_name} </th> <th> ${i.user2_name}</th> <th> ${i.multi_winner}</tr>`);
-    }
-    }
         $("#central").hide();
-    $("#statsTable").show();
-    });
+        $("#statsTable").show();
+      });
     });
   });
 
-    $.get("/gamesSorted",function(array){
-    $.get("/myusername", function(username){
-      $("#stats").click(function(){
-        array.forEach(function(element, index){
-          if (username === element.username){
+  $.get("/gamesSorted", function(array) {
+    $.get("/myusername", function(username) {
+      $("#stats").click(function() {
+        array.forEach(function(element, index) {
+          if (username === element.username) {
             rank = index + 1;
           }
         });
-        $('.numWins').text(`Your rank is: ${rank}`);
+        $(".numWins").text(`Your rank is: ${rank}`);
+      });
     });
-    });
-    });
+  });
 });
